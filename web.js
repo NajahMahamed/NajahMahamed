c8f8bf955e1c4621b93a75743882f5cd1a570347
Najah Mahamed <najah.mm@hotmail.co.uk>
2021-06-09 14:23:53 +0100

    update

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ if (string == "codenation" && rank == 1)
       { console.log(`Both ${string} and the ranking ${rank} are correct`);    
 } elseif (string == "codenation" && rank != 1) 
          { console.log(`${string} is correct but the ranking ${rank} isn’t`);  
-} else{
+         } else {
             console.log(`Both ${string} and/or the ranking ${rank} are wrong`);  
            } 
- dataChecker ("codenation", 1); 
+ console.log dataChecker ("codenation", 1);
