d44904f1d43b4deb1f86a1b7255c9f0cfdaf1827
Najah Mahamed <najah.mm@hotmail.co.uk>
2021-06-11 17:14:56 +0100

    update

--- web.js
+++
@@ -1,9 +0,0 @@
-const dataChecker = (string, rank) => { 
-if (string == "codenation" && rank == 1) 
-      { console.log(`Both ${string} and the ranking ${rank} are correct`);    
-} elseif (string == "codenation" && rank != 1) 
-         { console.log(`${string} is correct but the ranking ${rank} isn’t`);  
-         } else {
-            console.log(`Both ${string} and/or the ranking ${rank} are wrong`);  
-           } 
- console.log dataChecker ("codenation", 1);
